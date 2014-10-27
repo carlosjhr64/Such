191b2dcbe989fc2731d89b261a67f19b532b5b1a
Fox <fox@localhost.localdomain>
2014-10-27 09:05:20 -0700

    isolating code segments for unit testing.

--- lib/such/thing.rb
+++ lib/such/thing.rb
@@ -1,31 +1,42 @@
 module Such
   module Thing
+
     PARAMETERS = {}
     def self.configure(conf)
       conf.each{|k,v| PARAMETERS[k]=v}
     end
 
-    def initialize(*parameters, &block)
+    def self.trace_method(obj, mthd, args)
+      $stderr.puts "#{obj.class}##{mthd}(#{[*args].join(',')})"
+    end
+
+    def self.trace_signal(obj, signal)
+      $stderr.puts "#{obj.class} links #{signal}"
+    end
+
+    def self.do_symbol(parameter, parameters)
+      if PARAMETERS.has_key?(parameter)
+        p = PARAMETERS[parameter]
+        (parameter[-1]=='!')? parameters.unshift(*p) : parameters.unshift(p)
+      else
+        warn "Warning: Such::PARAMETERS[#{parameter}] not defined"
+      end
+    end
+
+    def self.do_parameters(parameters)
       container, arguments, methods, signals = nil, [], {}, {}
       while parameter = parameters.shift
         case parameter
         when Symbol
           # Symbols are expected to translate to something else.
-          if PARAMETERS.has_key?(parameter)
-            p = PARAMETERS[parameter]
-            (parameter[-1]=='!')? parameters.unshift(*p) : parameters.unshift(p)
-          else
-            warn "Warning: Such::PARAMETERS[#{parameter}] not defined"
-          end
+          Thing.do_symbol(parameter, parameters)
         when Array
           # Arrays are added to the Thing's arguments list.
           arguments += parameter
         when Hash
           # Hashes are expected to be a symbol list of methods on Thing with respective arguments.
-          parameter.each do |k,v|
-            # It's possible to override a previously defined method with new arguments.
-            methods[k]=v
-          end
+          # It's possible to override a previously defined method with new arguments.
+          parameter.each{|k,v| methods[k]=v}
         when String
           # Typically a signal: Thing#signal_connect(signal){|*emits| block.call(*emits)}
           signals[parameter] = true
@@ -34,51 +45,53 @@ module Such
           container = parameter
         end
       end
+      return container, arguments, methods, signals
+    end
 
-      super(*arguments)
+    def self.into(obj, container, mthd=:add, *args)
+      if container
+        unless mthd.class==Symbol and container.respond_to?(mthd)
+          raise "Need container & method. Got #{container.class}##{mthd}(#{obj.class}...)"
+        end
+        Thing.trace_method(container, mthd, [obj.class,*args]) if $VERBOSE
+        container.method(mthd).call(obj, *args)
+      else
+        warn "Warning: Container for #{self.class} not given."
+      end
+    end
 
+    def self.do_method(obj, mthd, args)
+      Thing.trace_method(obj, mthd, args) if $VERBOSE
+      m = obj.method(mthd)
+      if m.arity == 1
+        # Assume user knows arity is one and means to iterate.
+        [*args].each{|arg| m.call(arg)}
+      else
+        m.call(*args)
+      end
+    end
+
+    def self.do_methods(obj, methods, container)
       # If user does not specify how to add to container, assume default way.
       methods[:into]=[] if container and !methods.has_key?(:into)
       methods.each do |mthd, args|
-        trace_method(mthd, args) if $VERBOSE
-        if mthd==:into
-          if container
-            into(container, *args)
-          else
-            warn "Warning: Container for #{self.class} not given."
-          end
-        else
-          m = method(mthd)
-          if m.arity == 1
-            # Assume user knows arity is one and means to iterate.
-            [*args].each{|arg| m.call(arg)}
-          else
-            m.call(*args)
-          end
-        end
+        (mthd==:into)? Thing.into(obj, container, *args) :
+                       Thing.do_method(obj, mthd, args)
       end
+    end
 
+    def self.do_links(obj, signals, block)
       signals.keys.each do |signal|
-        trace_signal(signal) if $VERBOSE
-        link(signal, block)
+        Thing.trace_signal(obj, signal) if $VERBOSE
+        obj.signal_connect(signal){|*emits| block.call(*emits)}
       end
     end
 
-    def link(signal, block)
-      signal_connect(signal){|*emits| block.call(*emits)}
-    end
-
-    def into(container, mthd=:add, *parameters)
-      raise "Need container & method" unless mthd.class==Symbol and container.respond_to?(mthd)
-      container.method(mthd).call(self, *parameters)
-    end
-
-    def trace_method(mthd, args)
-      $stderr.puts "#{self.class.name}##{mthd}(#{args})"
-    end
-
-    def trace_signal(signal)
-      $stderr.puts "#{self.class.name} links #{signal}"
+    def initialize(*parameters, &block)
+      container, arguments, methods, signals = Thing.do_parameters(parameters)
+      super(*arguments)
+      Thing.do_methods(self, methods, container)
+      Thing.do_links(self, signals, block)
     end
   end
 end
