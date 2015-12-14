a115639ff13a051a1cdf338c783fd86d170558d0
carlosjhr64 <carlosjhr64@gmail.com>
2015-12-14 14:10:35 -0800

    Now works with ruby 2.2.3, I hope.

--- lib/such/such.rb
+++ lib/such/such.rb
@@ -1,9 +1,15 @@
 module Such
   def self.subclass(clss, sprclss="Gtk::#{clss}", body='include Such::Thing')
-    eval <<-CODE
-    class #{clss} < #{sprclss}
-      #{body}
-    end
+    code = <<-CODE
+class #{clss} < #{sprclss}
+  #{body}
+end
     CODE
+    begin
+      eval code
+    rescue Exception
+      warn code
+      raise $!
+    end
   end
 end
--- lib/such/things.rb
+++ lib/such/things.rb
@@ -1,11 +1,25 @@
 module Such
   module Things
-    def self.list(clss=Gtk::Widget)
-      ObjectSpace.each_object(Class).select{|k| k < clss}
+    def self.list(klss)
+      ObjectSpace.each_object(Class).select{|k| k < klss}
+    end
+
+    def self.in(klass)
+      Things.list(klass).each do |clss|
+        begin
+          Such.subclass(clss.name.sub(/^.*::/, ''))
+        rescue Exception
+          warn "#{$!.class}:\t#{clss}"
+        end
+      end
     end
 
     def self.gtk_widget
-      Things.list.each{|clss| Such.subclass(clss.name.sub('Gtk::',''))}
+      Things.in(Gtk::Widget)
+    end
+
+    def self.tk_tile
+      Things.in(Tk::Tile)
     end
   end
 end
