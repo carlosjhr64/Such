39c2d4bfb13256cb1bd176009845318a92b282ba
carlosjhr64 <carlosjhr64@gmail.com>
2017-07-31 14:57:34 -0700

    very minor edits

--- lib/such.rb
+++ lib/such.rb
@@ -2,9 +2,9 @@ module Such
   VERSION = '0.3.0'
 end
 require 'such/thing'
-require 'such/part'
 require 'such/such'
 require 'such/things'
+require 'such/part'
 require 'such/parts'
 # Requires:
 #`ruby`
--- lib/such/such.rb
+++ lib/such/such.rb
@@ -1,9 +1,9 @@
 module Such
   def self.subclass(clss, sprclss="Gtk::#{clss}", body='include Such::Thing')
     code = <<-CODE
-class #{clss} < #{sprclss}
-  #{body}
-end
+      class #{clss} < #{sprclss}
+        #{body}
+      end
     CODE
     begin
       eval code
