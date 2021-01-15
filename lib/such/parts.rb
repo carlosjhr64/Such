990427095945f41362bf3bd56d8a458cfc088eff
carlosjhr64 <carlosjhr64@gmail.com>
2021-01-15 10:50:45 -0800

    some deep metaprogramming in tc_parts

--- lib/such/parts.rb
+++ lib/such/parts.rb
@@ -12,11 +12,9 @@ module Such
           raise "Plugs must have the form key_class: #{plug}"
         end
       end
-      Such.subclass(part, thing, include: Such::Part, attr_accessor: plugs) do
-        def self.plugs
-          [*plugs]
-        end
-      end
+      subklass = Such.subclass(part, thing, include: Such::Part, attr_accessor: plugs)
+      subklass.singleton_class.class_eval{ define_method(:plugs){plugs} }
+      return subklass
     end
   end
 end
