b812e5c559fbadf215d80387fc2cb459b95d9685
CarlosJHR64 <carlosjhr64@gmail.com>
2023-01-02 09:43:23 -0800

    Autoloading

--- lib/such.rb
+++ lib/such.rb
@@ -1,10 +1,10 @@
 module Such
   VERSION = '2.0.230102'
+  require 'such/such'
+  autoload :Thing,  'such/thing.rb'
+  autoload :Things, 'such/things.rb'
+  autoload :Part,   'such/part.rb'
+  autoload :Parts,  'such/parts.rb'
 end
-require 'such/such'
-require 'such/thing'
-require 'such/things'
-require 'such/part'
-require 'such/parts'
 # Requires:
 #`ruby`
