c1b4884b53bfe1de4f476d5629c11065cb0ae53d
CarlosJHR64 <carlosjhr64@gmail.com>
2023-01-02 09:52:16 -0800

    Reviewed Such::Things

--- lib/such/things.rb
+++ lib/such/things.rb
@@ -1,19 +1,20 @@
 module Such
   module Things
-    def self.list(superklass)
-      ObjectSpace.each_object(Class).select{|klass| klass < superklass}
+    def self.list(super_class)
+      ObjectSpace.each_object(Class).select{_1 < super_class}
     end
 
-    def self.subclass(klass)
-      Such.subclass(klass.name.sub(/^.*::/,'').to_sym, klass, include: Such::Thing)
+    def self.subclass(sub_class)
+      Such.subclass(sub_class.name.sub(/^.*::/,'').to_sym, sub_class,
+                    include: Such::Thing)
     end
 
-    def self.in(superklass)
-      Things.list(superklass).each do |klass|
+    def self.in(super_class)
+      Things.list(super_class).each do |sub_class|
         begin
-          Things.subclass(klass)
+          Things.subclass(sub_class)
         rescue
-          $stderr.puts "#{$!.class}:\t#{superklass}" if $VERBOSE
+          $stderr.puts "#{$!.class}:\t#{super_class}" if $VERBOSE
         end
       end
     end
