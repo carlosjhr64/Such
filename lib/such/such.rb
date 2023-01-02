997175ef9f7ca64285dfa698302d3877ae1836b6
CarlosJHR64 <carlosjhr64@gmail.com>
2023-01-02 09:26:18 -0800

    Review and comment on Such.subclass

--- lib/such/such.rb
+++ lib/such/such.rb
@@ -1,8 +1,13 @@
 module Such
-  def self.subclass(name, klass, **kw, &block)
-    subklass = const_set(name, Class.new(klass))
-    kw.each{|method, args| subklass.public_send(method, *args)}
-    subklass.class_eval(&block) if block
-    return subklass
+  def self.subclass(name, super_class, **including, &block)
+    # See documentation for Class#new
+    sub_class = const_set(name, Class.new(super_class))
+    # To be able to say like:
+    #   include Such::Thing
+    #   attr_accessor :attribute2, :attribute2
+    including.each{|these, mods| sub_class.public_send(these, *mods)}
+    # And to explicitly define methods in the subclass:
+    sub_class.class_eval(&block) if block
+    return sub_class
   end
 end
